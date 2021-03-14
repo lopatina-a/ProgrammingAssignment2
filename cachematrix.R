137ff8aedab72ea0b6eef06ebfd55a360615d0dd
user1 <user1@user1s-MacBook-Air.local>
2021-03-14 23:56:40 +0300

    Solution 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## functions do
 
 
-
+#The first function, makeCacheMatrix creates a special “matrix”, which is really a list containing a function to:set the value of the matrix, get the value of the matrix, set the value of the inverse, get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
   k <- NULL
   set <- function(y) {
@@ -20,7 +20,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-
+#This function computes the inverse of the special “matrix” returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
   k <- x$getinverse()
   if (!is.null(k)) {
